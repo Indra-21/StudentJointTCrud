686c346bf7945c83e2f2e304de279ae30d802d0a
Indra Saputra <indrasaputratra21@gmail.com>
2021-11-26 23:50:26 +0700

    BE

---
+++ src/main/java/com/student/studentcrud/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.student.studentcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
